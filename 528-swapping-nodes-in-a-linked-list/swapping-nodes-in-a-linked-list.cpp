5593750603272e4bbcd30f8aedfc378f7e947e0f
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2023-11-11 10:51:14 +0530

    Time: 408 ms (53.93%) | Memory: 180.5 MB (52.86%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -1,44 +1,40 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* swapNodes(ListNode* head, int k) {
-        int count=1;
+    ListNode* getNode(ListNode* head,int idx){
         ListNode* temp=head;
-        while(count<k && temp!=NULL){
+        for(int i=1;i<=idx;i++){
             temp=temp->next;
-            count++;
         }
-        int val1=temp->val;
-        temp=head;
-        int len=0;
+        return temp;
+    }
+    ListNode* swapNodes(ListNode* head, int k) {
+       int len=0;
+       ListNode* temp=head;
+        if(head==NULL)  return NULL;
         while(temp!=NULL){
-            temp=temp->next;
-            len++;
-        }
-        count=1;
-        temp=head;
-        while(count<=(len-k) && temp!=NULL){
-            temp=temp->next;
-            count++;
-        }
-        int val2=temp->val;
-        temp->val=val1;
-        temp=head;
-        count=1;
-        while(count<k && temp!=NULL){
-            temp=temp->next;
-            count++;
-        }
-        temp->val=val2;
+           temp=temp->next;
+           len++;
+       } 
+       if(len==2){
+        swap(head->val,head->next->val);
         return head;
+       }
+       int i=0;
+       int j=len-1;
+       while(i<len){
+       if(i==(k-1) && j==(len-k)){
+           ListNode* left=getNode(head,i);
+           ListNode* right=getNode(head,j);
+           int t=left->val;
+           left->val=right->val;
+           right->val=t;
+           return head;
+       }
+       else{
+            i++;
+            j--;
+       }
+       }
+      return head;
     }
 };
