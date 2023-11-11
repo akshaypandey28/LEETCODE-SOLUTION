04afa8bb4ac16d00184f6ec646faa8f56f51a941
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2023-11-11 10:55:09 +0530

    Time: 362 ms (99.86%) | Memory: 180.3 MB (87.21%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -15,10 +15,10 @@ public:
            temp=temp->next;
            len++;
        } 
-       if(len==2){
-        swap(head->val,head->next->val);
-        return head;
-       }
+    //    if(len==2){
+    //     swap(head->val,head->next->val);
+    //     return head;
+    //    }
        int i=0;
        int j=len-1;
        while(i<len){
