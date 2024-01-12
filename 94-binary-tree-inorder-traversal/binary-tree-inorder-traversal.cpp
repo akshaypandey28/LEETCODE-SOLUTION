fdd366dcc889d4bf07acecb7930f71bda2d84046
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-01-13 00:45:11 +0530

    Time: 3 ms (34.77%) | Memory: 8.7 MB (78.74%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,17 +11,31 @@
  */
 class Solution {
 public:
-    void in(TreeNode* root,vector<int> &v){
-    if(root==NULL){
-        return ;
-    }
-    in(root->left,v);
-    v.push_back(root->val);
-    in(root->right,v);
-}
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> v;
-        in(root,v);
-        return v;
+        vector < int > inorder;
+        TreeNode* curr=root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{ //find predecessor
+                TreeNode* prev=curr->left;
+                while(prev->right!=NULL && prev->right!=curr){ // prev->right!=curr because last me 
+                //right most node ko us time ke particular root se link kr diya ja rha hai curr->right
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        return inorder;
     }
 };
