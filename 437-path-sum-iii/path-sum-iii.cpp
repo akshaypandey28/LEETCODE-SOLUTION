ada655970b15e3e1cd1200245939240ed8c94457
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-01-18 22:17:29 +0530

    Time: 20 ms (52.69%) | Memory: 16.5 MB (48.36%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -11,18 +11,18 @@
  */
 class Solution {
 public:
-    void sum(TreeNode* root, int targetSum,long long add,int &count){
+    void sum(TreeNode* root,int targetSum,long long add,int &count){
         if(root==NULL) return ;
-        add=add+root->val;
+        add+=root->val;
         if(add==targetSum) count++;
-        sum(root->left,targetSum,add,count); 
-        sum(root->right,targetSum,add,count); 
+        sum(root->left,targetSum,add,count);
+        sum(root->right,targetSum,add,count);
     }
-    int pathSum(TreeNode* root, int targetSum) { //to add the sums of every nodepath
+    int pathSum(TreeNode* root, int targetSum) {
         if(root==NULL) return 0;
         int count=0;
         sum(root,targetSum,0,count);
-        count+=pathSum(root->left,targetSum) + pathSum(root->right,targetSum);
+        count+=pathSum(root->left,targetSum)+pathSum(root->right,targetSum);
         return count;
     }
 };
