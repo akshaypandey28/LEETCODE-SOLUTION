f86edae6f4188ba5e31e556c046bc3da31e2ab8c
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-03-03 00:59:07 +0530

    Time: 11 ms (52.63%) | Memory: 16.1 MB (31.51%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,27 @@
+#define pp  pair<int,int>
+class Solution {
+public:
+    
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int> row(n,0); // row array
+        vector<int> col(m,0); // col array
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(row[i]==1|| col[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        return ;
+    }
+};
