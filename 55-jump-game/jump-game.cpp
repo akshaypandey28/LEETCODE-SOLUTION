1481ba8ba9902188ed83e9b9c5664a274a7e6b41
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-03-08 23:23:11 +0530

    Time: 540 ms (12.97%) | Memory: 51.3 MB (15.76%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<bool> dp;
+    bool f(vector<int> &nums,int n){
+        bool maintain=false;
+        if(dp[n-2]==true) maintain=true; 
+        for(int i=n-3; i>=0; i--){
+            if(nums[i]>=(n-1-i)){
+                dp[i]=true;
+            }
+            else{
+                int temp=nums[i];
+                int j=i+1;
+                //bool flag=false;
+                while(temp--){
+                    if(dp[j]==true){
+                        //flag=true;
+                        dp[i]=true;
+                        break;
+                    }
+                    j++;
+                }
+            }
+        }
+        return dp[0];
+    }
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return true;
+        dp.clear();
+        dp.resize(10005,0);
+        dp[n-1]=1;
+        if(nums[n-2]>=1) dp[n-2]=1;
+        return f(nums,n);
+    }
+};
