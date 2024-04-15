b8fb2932652b489c4ee9898fc838bfc54c054194
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-04-15 18:34:32 +0530

    Time: 7 ms (30.79%) | Memory: 21.4 MB (5.66%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -7,11 +7,27 @@ public:
 
         if(dp[r][c]!=-1) return dp[r][c];
 
-        return dp[r][c]=triangle[r][c] + min(f(triangle,r+1,c),f(triangle,r+1,c+1));
+        return dp[r][c]=triangle[r][c] + min(f(triangle,r+1,c),f(triangle,r+1,c+1));  //first is down 
+        //second is down and then right
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         dp.clear();
         dp.resize(205,vector<int>(205,-1));
         return f(triangle,0,0);
+
+        //bottom up solution
+        // base case
+        // for(int i = 0; i < grid[rows-1].size(); i++) {
+        //     dp[rows-1][i] = grid[rows-1][i];
+        // }
+        
+        // for(int r = rows - 2; r >= 0; r--) {
+        //     for(int c = 0; c < grid[r].size(); c++) {
+        //         dp[r][c] = grid[r][c] + min(dp[r+1][c], dp[r+1][c+1]);
+        //     }
+        // }
+        
+        
+        // return dp[0][0];
     }
 };
