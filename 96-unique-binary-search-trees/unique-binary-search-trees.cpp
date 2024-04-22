48ccb653e72d27146eb7694700d47a84dcc29816
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-04-22 15:12:43 +0530

    Time: 3 ms (31.12%) | Memory: 7 MB (83.17%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> dp;
-    int f(int i) {
-        if(i == 0 or i == 1) return 1;
-        if(i == 2) return 2;
-        if(dp[i] != -1) return dp[i];
-        int sum = 0;
-        for(int k = 1; k <= i; k++) { //this loop is for because when i=3 comes then i need to make root from 
-            //1 to i that is upto 3
-            // k -> potential root
-            sum += f(k-1)*f(i-k);
-        }
-        return dp[i] = sum;
-    }
     int numTrees(int n) {
         dp.clear();
-        dp.resize(30, -1);
-        return f(n);
+        dp.resize(30, 0);
+        dp[0] = dp[1] = 1;
+        dp[2] = 2;
+        //f(n) nikalna hai to f(n-1) f(n-2) etc sare answer hone chahiye
+        for(int i = 3; i <= n; i++) { // order
+            // f(i)
+            for(int k = 1; k <= i; k++) {
+            // k -> potential root
+                dp[i] += dp[k-1]*dp[i-k];
+            }
+        }
+        return dp[n];
     }
 };
