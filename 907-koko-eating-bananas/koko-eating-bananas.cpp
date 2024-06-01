01abbdcf032d5b70ca0de5857f62f6c6ca2dfd95
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-06-01 20:20:17 +0530

    Time: 27 ms (82.87%) | Memory: 22.1 MB (15.05%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int maxi(vector<int> v){
-        int ans=v[0];
-        for(int i=1; i<v.size(); i++) {
-            if(v[i]>ans) ans=v[i];
+       int ans = INT_MIN;
+        for(int i = 0; i < v.size(); i++){
+            ans = max(ans, v[i]);
         }
         return ans;
     }
-    long long calc(vector<int> v,int k){ //k bananas per hour
-        long long totalH=0;
-        for(int i=0; i<v.size(); i++){
-            totalH+=ceil((double)v[i]/(double)k);
-        }
-        return totalH;
+    long long calc(vector<int>& piles, int k) {
+    long long total = 0;
+    for (int i = 0; i < piles.size(); i++) {
+        total += ceil((double)piles[i] / k);
     }
+    return total;
+}
     int minEatingSpeed(vector<int>& piles, int hours) {
         int l=1,h=maxi(piles); //represents k range that is k bananas per hour
-        int ans=0;
-        while(l<h){
-            int mid=(l+h)/2;
-            int kVal=calc(piles,mid);
+        int ans=h;
+        while(l<=h){
+           int mid = l + (h - l) / 2;
+            long long kVal=calc(piles,mid);
             if(kVal<=hours) { //ho skta hai kVal=5 aya hai but 4 might be our answer that's why right side discarded
-                h=mid;
-                ans=mid;
+                h=mid-1;
+                ans=min(ans,mid);
             }
             else l=mid+1; //          
         }
