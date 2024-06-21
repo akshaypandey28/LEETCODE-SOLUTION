13829ff4147b71992a64c1c62d23e14e385603a9
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-06-21 12:16:34 +0530

    Time: 15 ms (33.39%) | Memory: 16.2 MB (91.69%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,29 +1,23 @@
 #define pp pair<int, int>
 class Solution {
 public:
-    // static bool check(const pp& p1, const pp& p2){
-        
-    //     if (p1.first == p2.first) {
-    //         return p1.second  < p2.second ;
-    //     }
-    //     // compare based on the integer value
-    //     return p1.first > p2.first;
-    //}
     vector<int> topKFrequent(vector<int>& nums, int k) {
         map<int,int> m;
         for(int i=0; i<nums.size(); i++) m[nums[i]]++;
 
-        priority_queue<pp> pq;
-
-        for(auto ele:m)  pq.push({ele.second,ele.first});
-
+        priority_queue<pp,vector<pp> ,greater<pp>> pq;
         vector<int> ans;
-        while(k--){
+        for(auto ele:m)  {
+            pq.push({ele.second,ele.first});
+            if(pq.size()>k) pq.pop();
+        }
+
+        
+        while(pq.empty()!=1){
             pp top=pq.top();
             ans.push_back(top.second);
             pq.pop();
         }
         return ans;
-        
     }
 };
