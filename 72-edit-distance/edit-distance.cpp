a4077c6f9fae49506a6a869e298feeb189bb17b2
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-07-12 16:52:02 +0530

    Time: 347 ms (5.06%) | Memory: 454.5 MB (5.13%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<vector<int>> dp;
     int f(string word1, string word2,int i,int j){
-        if(i == word1.size()) return word2.size() - j; // Return the remaining characters of word2
-        if(j == word2.size()) return word1.size() - i;
+        if(i == word1.size()) return word2.size() - j; // Return the remaining characters of word2 means addition in word1
+        if(j == word2.size()) return word1.size() - i; //deletion of word1 characters
         if(dp[i][j]!=-1) return dp[i][j];
         if(word1[i]==word2[j]){
             return dp[i][j]=f(word1,word2,i+1,j+1);
