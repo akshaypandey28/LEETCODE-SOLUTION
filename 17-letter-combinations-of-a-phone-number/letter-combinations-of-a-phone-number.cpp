84e35821c3cbcbf684e7457b2b862749dad7fa9c
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-07-13 15:00:58 +0530

    Time: 2 ms (40.62%) | Memory: 8.7 MB (16.05%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> ans;
+    vector<string> v={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    void gen(string d,int i,string s){
+        if(i==d.size()){
+            ans.push_back(s);
+            return ;
+        }
+        int digit=d[i]-'0';
+        for(int j=0; j<v[digit].size(); j++){
+            gen(d,i+1,s+v[digit][j]);
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        if(digits.size()==0) return {};
+        string s="";
+        gen(digits,0,s);
+        return ans;
+    }
+};
