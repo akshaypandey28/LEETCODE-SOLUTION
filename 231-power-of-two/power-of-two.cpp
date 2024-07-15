dfd34378cfeb16be08f5bf1d7c2b88026ccdc340
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-07-15 14:51:52 +0530

    Time: 5 ms (20.45%) | Memory: 7.6 MB (9.88%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int count=0;
+        while(n>0){
+            if(n%2==1){
+            count++;
+            }
+            n=n/2;
+        }
+       return count==1;
+    }
+};
