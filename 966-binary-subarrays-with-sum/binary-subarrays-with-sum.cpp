75669c8edd1058f57dab85afbe03a8f28060723c
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-07-26 11:05:19 +0530

    Time: 26 ms (46.64%) | Memory: 33.6 MB (34.59%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -21,3 +21,25 @@ public:
         return f(nums,goal) - f(nums,goal-1);
     }
 };
+
+/* 
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int ans = 0;
+        int sum = 0;
+        unordered_map<int, int> prefixSumCount;
+        prefixSumCount[0] = 1;  // zero sum always exist
+
+        for (int num : nums) {
+            sum += num;
+            if (prefixSumCount.find(sum - goal) != prefixSumCount.end()) {
+                ans += prefixSumCount[sum - goal];
+            }
+            prefixSumCount[sum]++;
+        }
+        
+        return ans;
+    }
+};
+ */
