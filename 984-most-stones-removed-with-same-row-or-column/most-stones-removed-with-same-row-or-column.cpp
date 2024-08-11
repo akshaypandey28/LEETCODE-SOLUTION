09fe836ef6bf08b721ee7c8e591118c33393c033
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-08-11 22:31:25 +0530

    Time: 20 ms (96.42%) | Memory: 21.9 MB (60.86%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,21 +1,27 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
-    int find(vector<int> &parent, int x) {
+    // DSU to find the number of connected component
+    // ans = number of nodes - number of connected component
+    // as in every connected component every node gets destroyed except one node which is not part of the answer
+    // so basically after these steps every connected component has 1 node if 5 connected component then every has 1, total 5 nodes remain undestroyed
+
+    int find(vector<int> &parent, vector<int> &rank, int x) {
+        // TX:(log*n);
+        // This method returns which group/cluster x belongs to
         if (parent[x] == x) return x;
-        return parent[x] = find(parent, parent[x]);
+        return parent[x] = find(parent, rank, parent[x]);
     }
 
     void Union(vector<int> &parent, vector<int> &rank, int a, int b) {
-        a = find(parent, a);
-        b = find(parent, b);
+        // TX:(log*n);
+        a = find(parent, rank, a);
+        b = find(parent, rank, b);
         if (a == b) return;
         if (rank[a] >= rank[b]) {
+            rank[a]++;
             parent[b] = a;
-            if (rank[a] == rank[b]) rank[a]++;
         } else {
+            rank[b]++;
             parent[a] = b;
         }
     }
@@ -27,8 +33,9 @@ public:
             maxC = max(maxC, ele[1]);
         }
         int n = maxR + maxC + 2; // Ensure enough space for all rows and columns
-        vector<int> parent(n), rank(n, 0);
-        for (int i = 0; i < n; ++i) parent[i] = i; // Properly initialize parent array
+        vector<int> parent(n);
+        vector<int> rank(n, 0);
+        for (int i = 0; i < n; ++i) parent[i] = i;
 
         for (auto ele : stones) {
             int nodeR = ele[0];
@@ -38,7 +45,7 @@ public:
 
         int count = 0;
         for (int i = 0; i < n; ++i) {
-            if (parent[i] == i && rank[i] > 0) {
+            if (parent[i] == i and rank[i]>0) { //this ensures that at any ith node if rank is zero then it not part of our answer and rank[i]>0 means at least one node has a parent[i]==i which helps for answer
                 count++;
             }
         }
