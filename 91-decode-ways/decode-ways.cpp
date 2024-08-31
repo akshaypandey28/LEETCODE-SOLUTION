366123efe50854101efff80f66e30606f310accc
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-08-31 11:53:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (52.65%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,28 +1,19 @@
+#define ll long long int
 class Solution {
 public:
-    vector<int> dp;
-    int fbu(string &str) {
-        dp.clear();
-        dp.resize(5005, 0);
-        dp[0] = (str[0] != '0');
-        if(str.size() > 1) {
-            if(str[1] != '0') dp[1] += dp[0];
-            if(str[1-1] - '0' > 0 and (str[0] - '0')*10 + (str[1] - '0') <= 26) dp[1]++;
-        }
-        for(int i = 2; i < str.size(); i++) {
-            int ans = 0;
-            if(str[i] - '0' > 0) {
-                ans+=dp[i-1];
-            } 
-            if(str[i-1]-'0' > 0 and (str[i-1] - '0')*10 + (str[i] - '0') <= 26) {
-                ans += dp[i-2];
-            }
-            dp[i] = ans;
-        }
-        return dp[str.size() - 1];
-
-}
+    vector<ll> dp;
+    int f(string &s,int i){
+        if(i>=s.size()) return 1;
+        if(dp[i]!=-1) return dp[i];
+        int ans=0;
+        if(s[i]-'0'!=0) ans+=f(s,i+1);
+        if(s[i]-'0' !=0 and (i+1)< s.size() and (s[i] - '0')*10 + (s[i+1] - '0') <= 26) ans += f(s, i+2);
+        return dp[i] = ans;
+    }
     int numDecodings(string s) {
-        return fbu(s);
+        if(s[0]=='0') return 0;
+        dp.clear();
+        dp.resize(105, -1);
+        return f(s,0);
     }
 };
