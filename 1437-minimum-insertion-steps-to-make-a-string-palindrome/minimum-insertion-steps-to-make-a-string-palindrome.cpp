0be53e2bf4f3b94935c45fde7c4df72ec9ff7af8
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-09-12 22:46:48 +0530

    Time: 16 ms (95.08%) | Memory: 8.2 MB (94.70%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minInsertions(string s1) {
+        int n = s1.size();
+        string s2 = s1;
+        reverse(s2.begin(), s2.end());  // Reverse s1 to get s2
+        
+        vector<int> curr(n + 1, 0), next(n + 1, 0);
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (s1[i] == s2[j]) {
+                    curr[j] = 1 + next[j + 1];
+                } else {
+                    curr[j] = max(next[j], curr[j + 1]);
+                }
+            }
+            next = curr;  // Move current row to next row for the next iteration
+        }
+        
+        int longest_palindromic_subsequence=next[0]; //whole code tells about the longest palindromic subsequence
+        //using longest common subsequence
+        return n-longest_palindromic_subsequence;
+    }
+};
