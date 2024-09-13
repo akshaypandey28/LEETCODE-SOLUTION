a5de9e1b347f7b2ce9759988cda6256f2b77949c
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-09-14 00:31:13 +0530

    Time: 6 ms (31.73%) | Memory: 14.6 MB (56.25%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int f(int i,int buy,vector<int> &prices){
-        if (i >= prices.size()) return 0;
-        if (dp[i][buy] != -1) return dp[i][buy];
-        int ans=INT_MIN;
-        ans=f(i+1,buy,prices); //avoid case no sell and no buy both included that's why but has no effect
-        if(buy==0) ans=max(-prices[i]+f(i+1,1,prices),ans ); //on ith day buy
-        else      ans=max(prices[i]+f(i+2,0,prices) , ans) ; //on ith day sell
-        return dp[i][buy]=ans;
-    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        dp.clear();
-        dp.resize(n, vector<int>(2, -1));
-        return f(0,0,prices);
+        vector<vector<int>> d(n+2,vector<int>(2,0));
+        for(int i=n-1; i>=0; i--){
+            for(int buy=1; buy>=0; buy--){
+                int ans=INT_MIN;
+                ans=d[i+1][buy]; //avoid case no sell and no buy both included that's why but has no effect
+                if(buy==0) ans=max(-prices[i]+d[i+1][1],ans ); //on ith day buy
+                else      ans=max(prices[i]+d[i+2][0] , ans) ; //on ith day sell
+                d[i][buy]=ans;
+            }
+        }
+        return d[0][0];
     }
 };
