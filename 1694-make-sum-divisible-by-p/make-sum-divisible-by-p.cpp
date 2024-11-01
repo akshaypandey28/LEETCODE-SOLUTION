b9dd7afe238a177dc38f6d165e1fde232de229d1
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-11-01 21:50:14 +0530

    Time: 88 ms (54.91%) | Memory: 87.8 MB (5.22%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
-        
+        //formula => target=curr-prev here curr is sum from to 0 to i that is prefix sum
+        //and prev is sum from 0 to j and j<=i
+        // prev=curr-target , so i have to look for prev sum which is stored in map using index store
+        //and answer is min(ans,i-index of prev) => (ans,i-m[prev])
         long long sum = 0;
         for (int i = 0; i < nums.size(); i++) sum += nums[i];
         int target = sum % p;
         if (target == 0) return 0;
 
-        unordered_map<int, int> m;
+        unordered_map<int, int> m; //<prev_sum,index>
         m[0] = -1;
-        int prefix = 0;
+        int curr = 0;
         int ans = nums.size();
         for (int i = 0; i < nums.size(); i++) {
-            prefix = (prefix + nums[i]) % p; // i want to find the subarray whose sum can become equal to target
-            int key = (prefix - target + p) % p; // Handling Negative Remainders
-            if (m.find(key) != m.end()) ans = min(ans, i - m[key]); // Check for the subarray
-            m[prefix] = i;
+            curr = (curr + nums[i]) % p; // i want to find the subarray whose sum can become equal to target
+            int prev = (curr - target + p) % p; // Handling Negative Remainders
+            if (m.find(prev) != m.end()) ans = min(ans, i - m[prev]); // Check for the subarray
+            m[curr] = i;
         }
         return (ans == nums.size() ? -1 : ans);
     }
