a649683d06336c1fba539198c7d2771d7f583ceb
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-11-02 13:31:58 +0530

    Time: 1 ms (34.60%) | Memory: 8.5 MB (55.38%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
@@ -14,12 +14,12 @@ public:
         int i = 0, j = vec1.size()-1; //s1 (vec1)
         int l = 0, r = vec2.size()-1; //s2 (vec2)
 
-        while(l < vec2.size() && i < vec1.size() && vec2[l] == vec1[i]) {
+        while(l < vec2.size() && i < vec1.size() && vec2[l] == vec1[i]) { //prefix match
             l++;
             i++;
         }
 
-        while(r >= l && vec2[r] == vec1[j]) {
+        while(r >= l && vec2[r] == vec1[j]) { //suffix match
             j--;
             r--;
         }
