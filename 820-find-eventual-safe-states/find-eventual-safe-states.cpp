d441bfd1f4f90efb63394b3c21dc7f83a370005b
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-11-10 15:40:50 +0530

    Time: 7 ms (87.45%) | Memory: 52.1 MB (78.20%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool dfsCheck(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& pathVis, vector<int>& check) {
+        vis[node] = 1;
+        pathVis[node] = 1;
+        check[node] = 0;
+
+        // traverse for adjacent nodes
+        for (auto it : adj[node]) {
+            // when the node is not visited
+            if (!vis[it]) {
+                if (dfsCheck(it, adj, vis, pathVis, check) == true) {
+                    check[node] = 0; // Retained as per your code
+                    return true;
+                }
+            }
+            // if the node has been previously visited
+            // but it has to be visited on the same path
+            else if (pathVis[it]) {
+                check[node] = 0; // Retained as per your code
+                return true;
+            }
+        }
+
+        check[node] = 1;
+        pathVis[node] = 0;
+        return false;
+    }
+
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        int V = adj.size();
+        vector<int> vis(V, 0), pathVis(V, 0), check(V, 0);
+
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfsCheck(i, adj, vis, pathVis, check); // Add the missing semicolon
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < V; i++) {
+            if (check[i] == 1) ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
