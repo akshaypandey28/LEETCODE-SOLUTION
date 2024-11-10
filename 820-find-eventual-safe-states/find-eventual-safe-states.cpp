3ec9e6f133d0db6a18217eae819d550816342ea8
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-11-10 16:04:20 +0530

    Time: 55 ms (24.30%) | Memory: 65.4 MB (32.89%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,47 +1,34 @@
 class Solution {
 public:
-    bool dfsCheck(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& pathVis, vector<int>& check) {
-        vis[node] = 1;
-        pathVis[node] = 1;
-        check[node] = 0;
-
-        // traverse for adjacent nodes
-        for (auto it : adj[node]) {
-            // when the node is not visited
-            if (!vis[it]) {
-                if (dfsCheck(it, adj, vis, pathVis, check) == true) {
-                    check[node] = 0;  //contain cycle on iteration through this node
-                    return true;
-                }
-            }
-            // if the node has been previously visited
-            // but it has to be visited on the same path
-            else if (pathVis[it]) {
-                check[node] = 0;  //contain cycle on iteration through this node
-                return true;
-            }
-        }
-
-        check[node] = 1; //part of the answer because this node doesn't contain any cycle throughout the iteration through this node
-        pathVis[node] = 0; //baktracking of marking the node path untracked
-        return false; //cycle not detected
-    }
-
     vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
-        int V = adj.size();
-        vector<int> vis(V, 0), pathVis(V, 0), check(V, 0);
+        int v=adj.size();
 
-        for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                dfsCheck(i, adj, vis, pathVis, check); 
+        //reverse the edges as in the question ask for outdegree then it converts into indegree
+        vector<vector<int>> graph(v);
+        vector<int> indegree(v,0);
+        for(int i=0; i<v; i++){
+            for(auto neighbour:adj[i]){
+                graph[neighbour].push_back(i);
+                indegree[i]++;
+            }
+        }
+        vector<int> ans;
+        queue<int> qu;
+        for(int i=0; i<v; i++){
+            if(indegree[i]==0) qu.push(i);
+        }
+
+        while(not qu.empty()) {
+            int node = qu.front();
+            ans.push_back(node);
+            qu.pop();
+            for(auto neighbour : graph[node]) {
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0)  qu.push(neighbour);
             }
         }
 
-        vector<int> ans;
-        for (int i = 0; i < V; i++) {
-            if (check[i] == 1) ans.push_back(i);
-        }
-
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
