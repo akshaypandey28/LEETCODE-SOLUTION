18a2601471f990751dc9b191f952fcf3edb84505
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-11-12 17:20:28 +0530

    Time: 224 ms (85.60%) | Memory: 177.8 MB (84.41%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxIncreasingSubarrays(vector<int>& nums) {
+        //Find the boundaries between strictly increasing subarrays and store it in the store
+        //with the count
         vector<int> store;
         int count=0;
         for(int i=0; i<nums.size()-1; i++){
@@ -11,19 +13,33 @@ public:
             }
         }
         store.push_back(count+1);
-        int ans=store[0]/2;
+        
+        //nums=[2,5,7,8,9,2,3,4,3,1]
+        //store=[5,3,1,1]
+
+        int ans=store[0]/2; //i can divide the store[0] into subarray strictly increasing 
+        //subarray that's why divide by 2
+
         for(int i=1; i<store.size(); i++){
-            if(store[i-1]>store[i]){
-                ans=max({ans,store[i],store[i-1]/2});
-            }
-            else if(store[i-1]==store[i]){
-                ans=max(ans,store[i]);
-            }
-            else{
-                ans=max({ans,store[i-1],store[i]/2});
-            }
+            //reduce the three if statements into a single line by finding max and min between 
+            //consecutive value
+
+            int maxi=max(store[i],store[i-1]);
+            int mini=min(store[i],store[i-1]);
+            ans=max({ans,mini,maxi/2});
+
+            /* 
+            if(store[i-1]>store[i]) ans=max({ans,store[i],store[i-1]/2}); //here value of 
+            //minimum is taken into consideration and half of maximum
+
+            else if(store[i-1]==store[i]) ans=max(ans,store[i]); ///two consecutive becomes 
+            //equal then ans is max of ans,store[i]
+
+            else ans=max({ans,store[i-1],store[i]/2}); //here value of 
+            //minimum is taken into consideration and half of maximum 
+            
+            */
         }
-        //if(ans==0) return 1;
         return ans;
     }
 };
