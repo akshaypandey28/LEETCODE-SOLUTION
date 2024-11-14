11d5ec09ebd2500668d3117f751ce350fc2b2554
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-11-14 20:06:52 +0530

    Time: 457 ms (50.57%) | Memory: 300.8 MB (31.22%) - LeetSync

---
+++ 3646-sum-of-good-subsequences/sum-of-good-subsequences.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int MAX = 100001;
+    int mod = 1000000007;
+
+    int sumOfGoodSubsequences(vector<int>& nums) {
+        vector<int> dp(MAX, 0); // dp for counting subsequences ending at any value
+        vector<int> sum(MAX, 0); // dp for computing the sum of subsequences ending at any value
+
+        for (int num : nums) {
+            dp[num] = (dp[num] + 1) % mod;
+            sum[num] = (sum[num] + num) % mod;
+
+            if (num - 1 >= 0 && dp[num - 1] > 0) {
+                dp[num] = (dp[num] + dp[num - 1]) % mod;
+                sum[num] = (sum[num] + sum[num - 1] + (long long)(dp[num - 1]) * num % mod) % mod;
+                // 1st =>khud ki wjh se sum, 2nd => sum of subsequences of num-1 and 3rd => no of
+                // subsequences of num-1 and multiply with num then it compute the sum of
+                // subsequences of value num
+            }
+
+            if (num + 1 < MAX && dp[num + 1] > 0) {
+                dp[num] = (dp[num] + dp[num + 1]) % mod;
+                sum[num] = (sum[num] + sum[num + 1] + (long long)(dp[num + 1]) * num % mod) % mod;
+                // 1st =>khud ki wjh se sum, 2nd => sum of subsequences of num+1 and 3rd => no of
+                // subsequences of num+1 and multiply with num then it compute the sum of
+                // subsequences of value num
+            }
+        }
+
+        long long ans = 0;
+        for (int i = 0; i < MAX; i++) ans = (ans + sum[i]) % mod;
+
+        return (int)ans;
+    }
+};
