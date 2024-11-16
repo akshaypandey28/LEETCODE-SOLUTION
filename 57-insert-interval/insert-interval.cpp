c80cd8ce1dc4f333564e3e3fa7e283d550202448
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-11-17 00:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (78.28%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,3 +1,37 @@
+//CodeStoryWithMik
+//Approach-2
+//T.C : O(n)
+//S.C : O(n) for result
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int i = 0;
+        vector<vector<int>> result;
+        
+        while(i < intervals.size()) {
+            if(intervals[i][1] < newInterval[0])  result.push_back(intervals[i]);
+
+            else if (intervals[i][0] > newInterval[1])  break;
+            
+            else {
+                //Overlap : merge them
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
+            }
+            i++;
+        }
+        
+        result.push_back(newInterval);
+        while(i < intervals.size()) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+        return result;
+    }
+};
+
+
+/* 
 //Approach-1 (TLE) 
 //T.C : (O(n^2))
 //S.C : O(1)
@@ -24,3 +58,4 @@ public:
         return intervals;
     }
 };
+ */
