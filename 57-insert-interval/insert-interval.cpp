c60df5574d4eba06de7b3e802196e385bb56f90e
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-11-16 23:30:03 +0530

    Time: 6 ms (11.04%) | Memory: 20.8 MB (44.41%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -5,7 +5,7 @@ public:
     }
     vector<vector<int>> insert(vector<vector<int>>& v, vector<int>& newInterval) {
         v.push_back(newInterval);
-        sort(v.begin(),v.end(),cmp);
+        sort(v.begin(),v.end());
         vector<vector<int>> ans;
         vector<int> temp=v[0];
         for(int i=1; i<v.size(); i++){
