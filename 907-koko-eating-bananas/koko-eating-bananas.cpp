091ca2cce92eb34a91be44e365cb5f4b24981455
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-11-19 17:34:10 +0530

    Time: 4 ms (89.93%) | Memory: 38.6 MB (6.65%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -15,13 +15,18 @@ public:
         return totalH;
     }
     int minEatingSpeed(vector<int>& piles, int hours) {
+        int ans;
         int l=1,h=maxi(piles); //represents k range that is k bananas per hour
         while(l<=h){
             int mid=(l+h)/2;
             long long kVal=calc(piles,mid);
-            if(kVal<=hours) h=mid-1; //ho skta hai kVal=5 aya hai but 4 might be our answer that's why right side discarded
+            //ho skta hai kVal=5 aya hai but 4 might be our answer that's why right side discarded
+            if(kVal<=hours) {
+                h=mid-1;
+                ans=mid; //mid might become the answer
+            } 
             else l=mid+1; //          
         }
-        return l;
+        return ans;
     }
 };
