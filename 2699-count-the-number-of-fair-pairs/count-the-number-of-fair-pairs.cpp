007510dab1ed2c6f9e670f537a43141ae5ef1b95
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-11-19 13:57:25 +0530

    Time: 61 ms (66.28%) | Memory: 60.5 MB (15.26%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -11,12 +11,13 @@ public:
         for(int i = 0; i < n; i++) { //O(n * logn)
              //o(n)
             int idx = lower_bound(begin(nums) + i + 1, end(nums), lower-nums[i]) - begin(nums); 
-            //Gives the firstc element NOT LESS than (lower - nums[i]) //log(n)
-            int x   = idx - 1 - i;
+            //Gives the first element NOT LESS than (lower - nums[i]) log(n)
+            int x   = idx - 1 - i; //count the no. of element which is less than lower-nums[i]
+            //between i+1 to idx
 
 
             idx = upper_bound(begin(nums) + i + 1, end(nums), upper-nums[i]) - begin(nums); //Gives
-            //the firstc element GREATER than (upper - nums[i]) log(n)
+            //the first element GREATER than (upper - nums[i]) log(n)
 
             int y = idx - 1 - i;
 
