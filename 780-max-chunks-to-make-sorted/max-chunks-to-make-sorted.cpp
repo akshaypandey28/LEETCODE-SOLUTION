76f33a505e33073a5b62bed7883eaf6e7097ea79
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-12-19 23:10:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (7.32%) - LeetSync

---
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -0,0 +1,34 @@
+//Approach-1 (Using prefixMax and SuffixMin)
+//T.C : O(n)
+//S.C : O(n)
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> prefixMax(begin(arr), end(arr));
+        vector<int> suffixMin(begin(arr), end(arr));
+
+        for (int i = 1; i < n; i++) {
+            prefixMax[i] = max(prefixMax[i - 1], prefixMax[i]);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(suffixMin[i + 1], suffixMin[i]);
+        }
+
+        //concept => i ke phle ka max and i se bad ka min means 
+        //i ke phle ka max means (i-1) idx
+        //i se bad ka min means i idx
+        int chunksCount = 0;
+        for (int i = 0; i < n; i++) {
+            int pehleKaMax = i > 0 ? prefixMax[i - 1] : -1;
+            int baadKaMin  = suffixMin[i];
+            
+            if(pehleKaMax < baadKaMin) {
+                chunksCount++;
+            }
+        }
+
+        return chunksCount;
+    }
+};
