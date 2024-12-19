4c453ec1dd4e49b99853703380185710a5338ad3
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-12-19 17:17:25 +0530

    Time: 31 ms (94.88%) | Memory: 104.4 MB (10.51%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -5,11 +5,17 @@ public:
         vector<int> ans;
         int n=spells.size();
         int m=potions.size();
+        int maxPotion = potions[m-1];
         for(int i=0; i<n; i++){
             int spell = spells[i];
             
             long long minPotion = ceil((1.0*success)/spell);
-            
+
+            if(minPotion > maxPotion) {
+                ans.push_back(0); //no pairs can be formed with the ith index
+                continue;
+            }
+
             int index = lower_bound(begin(potions), end(potions), minPotion) - begin(potions);
             
             ans.push_back(m-index); //denotes the no. of pairs can be made with ith index
