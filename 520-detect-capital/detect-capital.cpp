445e96fef21640a7e3ddbf8c629ddd43d374db3a
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-12-22 22:46:08 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (36.10%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -7,11 +7,12 @@ public:
             if(isupper(ch)) countCapitals++;
         }
         
-        if(countCapitals == 0) return true;
+        if(countCapitals == 0) return true; //means all are small
         
-        if(countCapitals == word.length()) return true;
+        if(countCapitals == word.length()) return true; //means all are capital
         
         if(countCapitals == 1 && isupper(word[0])) return true;
+        //means first word is capital and rest are small
         
         return false;
         
