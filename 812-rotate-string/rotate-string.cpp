4acc4fa3198f4a721269693c92a0b86388571ea3
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-12-23 22:53:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (23.77%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size()) return false;
-        goal=goal+goal;
-        if(goal.find(s)!=-1) return true;
+        s=s+s;
+        if(s.find(goal)!=-1) return true;
         return false;
     }
 };
