cb0322ed2f49825682b8f15d8ee2113ebaa92222
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-12-30 15:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (7.71%) - LeetSync

---
+++ 1033-broken-calculator/broken-calculator.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int f(int startValue, int target) {
+        if (startValue <= target) 
+            return target - startValue; // If startValue is lesser, subtract the difference.
+        
+        if (startValue % 2 == 0)
+            return 1 + f(startValue/2, target); // If startValue is even, divide it by 2.
+        else
+            return 1 + f(startValue+1, target); // If startValue is odd, increment it.
+    }
+    
+    int brokenCalc(int startValue, int target) {
+        //instead of going from startValue to target , go for reverse target to startValue
+        //(*2,-1) changes to (/2,+1)
+        return f(target, startValue);
+    }
+};
