4cfe50f493a15b36948e0db9b4a3acbdb7a18912
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2024-12-31 10:47:12 +0530

    Time: 1 ms (36.20%) | Memory: 13.3 MB (40.49%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,33 +1,24 @@
-//Approach-1 (Recursion + Memoized)  [Time : O(max_day)] [Space : O(n)] //n = size of days vector
 class Solution {
 public:
-    int t[366];
-    int memoized(vector<int>& days, vector<int>& costs, int& n, int idx) {
-        if(idx >= n) return 0; //you can't travel, so no cost
-                
-        if(t[idx] != -1) return t[idx];
-        
-        //if i take 1-day pass at idx
-        int cost_1 = costs[0] + memoized(days, costs, n, idx+1);
-        
-        //if i take 7-day pass at idx
-        int i = idx;
-        while(i < n && days[i] < days[idx]+7) i++;
-        
-        int cost_7 = costs[1] + memoized(days, costs, n, i);
-        
-        //if i take 30-day pass at idx
-        int j = idx;
-        while(j < n && days[j] < days[idx]+30) j++;
-        
-        int cost_30 = costs[2] + memoized(days, costs, n, j);
-        
-        return t[idx] = min({cost_1, cost_7, cost_30});
-    }
-    
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        memset(t, -1, sizeof(t));
-        int n = days.size();
-        return memoized(days, costs, n, 0);
+        int n=days.size();
+        vector<int> dp(n+1,0);
+        for(int i=n-1; i>=0; i--){
+            //1 day
+            int cost_1=costs[0]+dp[i+1];
+
+            //7 day
+            int j=i; 
+            while(j<n and days[j] < days[i]+7) j++;
+            int cost_7=costs[1]+dp[j];
+
+            //30 day
+            j=i;
+            while(j<n and days[j] < days[i]+30) j++;
+            int cost_30=costs[2]+dp[j];
+
+            dp[i]=min({cost_1,cost_7,cost_30});
+        }
+        return dp[0];
     }
 };
