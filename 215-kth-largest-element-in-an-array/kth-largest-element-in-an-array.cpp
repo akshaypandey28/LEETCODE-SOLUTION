1c0adc2758ca486a87029d04b14847043cb77d73
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-01-10 11:56:27 +0530

    Time: 37 ms (43.36%) | Memory: 61.6 MB (40.90%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -9,3 +9,62 @@ public:
         return pq.top();
     }
 };
+
+
+/* 
+//Approach-3 (Using QuickSelect Algorithm
+//Time complexity: O(n) on average, O(n2) in the worst case (See video above for explanation)
+class Solution {
+public:
+    
+    int partition_algo(vector<int>& nums, int L, int R) {
+        
+        int P = nums[L];
+        int i = L+1; //0
+        int j = R; //0
+        
+        while(i <= j) {
+            
+            if(nums[i] < P && nums[j] > P) {
+                swap(nums[i], nums[j]);
+                i++;
+                j--;
+            }
+            
+            if(nums[i] >= P) i++;
+            
+            if(nums[j] <= P) j--;
+            
+        }
+        
+        swap(nums[L], nums[j]);
+        return j; //P is at jth index
+    }
+    
+    int findKthLargest(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        int L = 0;
+        int R = n-1;
+        
+        int pivot_idx = 0;
+        
+        //kth largest pivot element - nums[k-1] (descending order me partition karenge)
+        
+        while(true) {
+            
+            pivot_idx = partition_algo(nums, L, R);
+            
+            if(pivot_idx == k-1) break;
+            
+            else if(pivot_idx > k-1) R = pivot_idx - 1;  //2nd largest , 4th largest
+
+            else  L = pivot_idx + 1;
+            
+        }
+        
+        
+        return nums[pivot_idx];
+        
+    }
+}; */
