f8756934a32aa4776ff96eb88dbc7e9cb9dbdea3
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-01-17 13:30:18 +0530

    Time: 1187 ms (82.35%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,11 +1,5 @@
 # Write your MySQL query statement below
-
-SELECT 
-    eu.unique_id AS unique_id,  -- Selecting the unique ID from EmployeeUNI table
-    e.name AS name              -- Selecting the employee's name
-FROM 
-    Employees e                 -- Main table
-LEFT JOIN 
-    EmployeeUNI eu              -- Joining with EmployeeUNI table
-ON 
-    e.id = eu.id;               -- Matching based on the employee's ID
+SELECT
+EmployeeUNI.unique_id, Employees.name
+FROM Employees
+LEFT JOIN EmployeeUNI on Employees.id = EmployeeUNI.id
