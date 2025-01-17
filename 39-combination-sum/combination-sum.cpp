ff4d2dda6cbdefae98bbc7567e08faccab2466b5
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-01-17 16:04:48 +0530

    Time: 90 ms (9.27%) | Memory: 67.5 MB (9.55%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -13,23 +13,23 @@ public:
 
         if(candidates[i] <= target) {
             v.push_back(candidates[i]);
+
             sum+=candidates[i];
+
             f(candidates,v,target,i,sum); //pick case when candidates[i] <= target
             //i+1 ke jgh i hai because next element pr jana mandatory nhi hai same element ko bar bar pick kiya ja skta hai
             //jb tk element ko na pick krne wali state na aa jaye
             sum-=candidates[i];
+            
             v.pop_back();
         }
-        //avoid
-        int j=i+1;
         
-        f(candidates,v,target,j,sum); //not pick case when candidates[i] <= target and for greater value than candidates[i]
+        f(candidates,v,target,i+1,sum); //not pick case when candidates[i] <= target and for greater value than candidates[i]
         
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> v;
         ans.clear();
-        //sort(candidates.begin(),candidates.end());
         f(candidates,v,target,0,0);
         
         return ans;
