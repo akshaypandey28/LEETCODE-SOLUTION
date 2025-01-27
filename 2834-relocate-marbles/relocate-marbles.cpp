ad19b93cf16a6dabeb4b77dd10fb2026a4c38762
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-01-27 16:31:49 +0530

    Time: 100 ms (78.57%) | Memory: 182.5 MB (82.47%) - LeetSync

---
+++ 2834-relocate-marbles/relocate-marbles.cpp
@@ -0,0 +1,24 @@
+//self
+//T.C => O( (m+n)logn )
+class Solution {
+public:
+    vector<int> relocateMarbles(vector<int>& nums, vector<int>& moveFrom, vector<int>& moveTo) {
+        unordered_set<int> s; //occupied position
+        int n=nums.size(),m=moveFrom.size();
+        for(int i=0; i<n; i++) s.insert(nums[i]); //O(n)
+
+        //O(m*logn)
+        for(int i=0; i<m; i++){ //O(m)
+            s.erase(moveFrom[i]); //erase all marbles on this position and move it to 
+            //moveTo][i] position
+            s.insert(moveTo[i]);
+
+            //erase and insert O(logn)
+        }
+        vector<int> ans;
+        for(auto ele:s) ans.push_back(ele);
+        sort(ans.begin(),ans.end()); //O(nlogn)
+        return ans;
+
+    }
+};
