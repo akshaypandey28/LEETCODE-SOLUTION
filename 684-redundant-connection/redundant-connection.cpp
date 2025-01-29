ea72732d0153bf756dacb785b77e139f94ca6f44
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-01-29 16:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (93.68%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.cpp
@@ -0,0 +1,62 @@
+//Approach-3 (DSU - Union Find with Path compression)
+//T.C : O(n * alpha(n))
+//S.C : O(n)
+class DSU {
+public:
+    vector<int> parent;
+    vector<int> rank;
+
+    DSU(int n) {
+        parent.resize(n+1); //1, 2, 3, 4, 5.., n
+        rank.resize(n+1);
+        for(int i = 1; i <= n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    int find(int x) {
+        if(x == parent[x]) return x;
+
+        return parent[x] = find(parent[x]); //path compression
+    }
+
+    void Union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x == y) return;
+        
+        if(rank[x] >= rank[y]) {
+            rank[x]++;
+            parent[y] = x;
+        } 
+        else {
+            parent[x] = y;
+            rank[y]++;
+        } 
+    }
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+
+        DSU dsu(n);
+        //T.C : DSU = alpha(n)
+
+        //T.C : O(n * alpha(n))
+        for(auto &edge : edges) { //O(n)
+            int u = edge[0];
+            int v = edge[1];
+
+            if(dsu.find(u) == dsu.find(v)) return edge;
+            
+            dsu.Union(u, v);
+        }
+
+        return {};
+    }
+};
+
