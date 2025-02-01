8aba6e715f617189fe771a3b51b6482e35d79109
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-02-01 11:59:06 +0530

    Time: 442 ms (34.03%) | Memory: 10.1 MB (15.52%) - LeetSync

---
+++ 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool sumOfNumberAndReverse(int n) {
+        
+        for(int i=0;i<=n/2;i++){
+            int x = i;
+            int y = n-i;
+            string a = to_string(y);
+            reverse(a.begin(),a.end());
+            y = stoi(a);
+            if(x==y)return true;
+        }
+        return false;
+    }
+};
