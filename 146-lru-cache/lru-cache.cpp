1a972fee19ecfd50f0031f6eeb1c68719e830c71
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-03-23 11:43:35 +0530

    Time: 125 ms (14.39%) | Memory: 186.6 MB (18.77%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,3 +1,5 @@
+//head->next is most recently used 
+//tail->prev is least recently used
 class LRUCache {
 public:
     class Node{
@@ -32,8 +34,6 @@ public:
         nextt -> prev = prevv;
     }
     void addNode(Node* newnode){
-
-        
 
         newnode -> next = head -> next;
         head -> next -> prev = newnode;
@@ -80,3 +80,57 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+
+ /* 
+ //Approach-1 (Brute Force)
+class LRUCache {
+public:
+    
+    vector<pair<int, int>> cache;
+    int n;
+    
+    LRUCache(int capacity) {
+        n = capacity;
+    }
+    
+    int get(int key) {
+        
+        for(int i = 0; i<cache.size(); i++) {
+            
+            if(cache[i].first == key) {
+                int val = cache[i].second;
+                
+                pair<int, int> temp = cache[i];
+                cache.erase(cache.begin()+i);
+                cache.push_back(temp);
+                
+                return val;
+            }
+            
+        }
+        
+        return -1;
+        
+    }
+    
+    void put(int key, int value) {
+        
+        for(int i = 0; i<cache.size(); i++) {
+            
+            if(cache[i].first == key) {
+                cache.erase(cache.begin()+i);
+                cache.push_back({key, value});
+                return;
+            }
+        }
+        
+        if(cache.size() == n) {
+            cache.erase(cache.begin());
+            cache.push_back({key, value});
+        } else {
+            cache.push_back({key, value});
+        }
+        
+    }
+};
+  */
