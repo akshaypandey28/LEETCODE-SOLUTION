335a140eefa9ea371b9425a5280a3dbc38c887c8
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-04-03 11:44:31 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (61.93%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
-    vector<int> dp;
-    int f(vector<int> &nums,int n){
-        for(int i=n-3; i>=0; i--){
-            if(nums[i]>=(n-1-i))  dp[i]=1;
-            else if(nums[i]==0) continue;
-            else{
-                int temp=nums[i]; //for traversing the number of steps
-                int j=i+1; //it is used for checking the value of dp from i+1
-                int ans=INT_MAX; //used to store the min steps from any point
-                while(temp--){
-                    if(dp[j]==0) {
-                        j++;
-                        continue;
-                    }
-                    if(dp[j]!=0){
-                        if(ans>dp[j]) ans=min(ans,dp[j]);
-                    }
-                    j++;
-                }
-                if(ans<INT_MAX) dp[i]=ans+1;
-            }
-        }
-        return dp[0];
-    }
     int jump(vector<int>& nums) {
+        int jumps=0;
+        int l=0,r=0;
         int n=nums.size();
-        if(n==1) return 0;
-        dp.clear();
-        dp.resize(10005,0);
-        dp[n-1]=0;
-        if(nums[n-2]>=1) dp[n-2]=1;
-        
-        int ans= f(nums,n);
-        for(int i=0; i<n; i++) cout<<dp[i]<<" ";
-        return ans;
+        while(r<n-1){
+            int farthest=0;
+            for(int i=l; i<=r; i++) farthest=max(farthest,i+nums[i]);
+            l=r+1;
+            r=farthest;
+            jumps++;
+        }
+        return jumps;
     }
 };
