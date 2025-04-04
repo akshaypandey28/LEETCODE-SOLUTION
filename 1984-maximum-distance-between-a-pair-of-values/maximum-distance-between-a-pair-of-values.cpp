ec6418c2439a64e810fc128c9bee8f7ab0ce4126
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-04-04 12:12:58 +0530

    Time: 11 ms (24.51%) | Memory: 102 MB (96.10%) - LeetSync

---
+++ 1984-maximum-distance-between-a-pair-of-values/maximum-distance-between-a-pair-of-values.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+//using binary search
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int m=nums1.size(),n=nums2.size();
+        int ans=0;
+        for(int i=0; i<m; i++){
+            int l=i,r=n-1;
+            int best_j=-1;
+            while(l<=r){
+                int mid=l+(r-l)/2;
+                if(nums2[mid]>=nums1[i]){
+                    best_j=mid;
+                    l=mid+1; // Try to find a larger j
+                }
+                else r=mid-1;
+            }
+            if(best_j!=-1) ans=max(ans,best_j-i);
+        }
+        return ans;
+    }
+};
