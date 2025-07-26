1f5af68f2d17a30b23ef16403a61367235814460
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-07-26 10:27:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (96.68%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.cpp
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        priority_queue<int> p(stones.begin(),stones.end());
-        while(p.size()>1){
-            int first=p.top();
-            p.pop();
-            int second=p.top();
-            p.pop();
-            if(first!=second) p.push(abs(second-first));
+        priority_queue<int> pq(stones.begin(),stones.end());
+        while(pq.empty()!=1){
+            if(pq.size()==1) return pq.top();
+            int y=pq.top();
+            pq.pop();
+            int x=pq.top();
+            pq.pop();
+            if(x!=y) pq.push(y-x);
         }
-        if(p.size()==0) return 0;
-        return p.top();
+        return 0;
     }
 };
