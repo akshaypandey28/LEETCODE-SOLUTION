6df3c22394c6fa2d5e216bdd41e2e539023496c1
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-07-27 11:32:58 +0530

    Time: 44 ms (17.27%) | Memory: 42.9 MB (15.73%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void f(vector<int>& candidates,vector<int> v,int target,int i,int sum){
-        if(sum==target){
+    void f(vector<int>& candidates,vector<int> v,int target,int i){
+        if(target==0){
             ans.push_back(v);
             return ;
         }
 
         if(i==candidates.size()) return ;
 
-        if(sum>target) return ;
+        if(target<0) return ;
 
         if(candidates[i] <= target) {
             v.push_back(candidates[i]);
 
-            sum+=candidates[i];
 
-            f(candidates,v,target,i,sum); //pick case when candidates[i] <= target
+            f(candidates,v,target-candidates[i],i); //pick case when candidates[i] <= target
             //i+1 ke jgh i hai because next element pr jana mandatory nhi hai same element ko bar bar pick kiya ja skta hai
             //jb tk element ko na pick krne wali state na aa jaye
-            sum-=candidates[i];
             
             v.pop_back();
         }
         
-        f(candidates,v,target,i+1,sum); //not pick case when candidates[i] <= target and for greater value than candidates[i]
+        f(candidates,v,target,i+1); //not pick case when candidates[i] <= target and for greater value than candidates[i]
         
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> v;
         ans.clear();
-        f(candidates,v,target,0,0);
+        f(candidates,v,target,0);
         
         return ans;
     }
