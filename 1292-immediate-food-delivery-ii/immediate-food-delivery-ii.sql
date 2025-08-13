352c0f7f755c2e2f96d6d5386afc494dab61bf6b
AKSHAY KUMAR PANDEY <akshaychaman2004@gmail.com>
2025-08-13 10:05:06 +0530

    Time: 747 ms (23.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select ROUND( SUM(IF(order_date=customer_pref_delivery_date , 1 , 0))*100 / count(distinct customer_id) , 2  ) as immediate_percentage 
+from Delivery
+where(customer_id,order_date) IN (
+    select customer_id , min(order_date) as first_order_date
+    from Delivery
+    group by customer_id
+)
